8c37082608db00c3eda123490ea9580069ba6b19
uwseoul <uwseoul@gmail.com>
2026-06-09 18:58:30 +0900

    feat: display app version in window title

--- src/QuotaBar.Win/MainWindow.xaml.cs
+++ src/QuotaBar.Win/MainWindow.xaml.cs
@@ -24,6 +24,7 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        Title = $"Quota Bar v{Assembly.GetExecutingAssembly().GetName().Version}";
         MouseLeftButtonDown += (_, __) => DragMove();
 
         var settings = _settingsService.Load();
